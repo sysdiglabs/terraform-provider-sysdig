b5fa92b30ad3376ef1043cd5ff9b4482b6598bff
Federico Barcelona <fede_rico_94@hotmail.com>
2020-11-05 10:00:46 +0100

    docs: Update examples

--- examples/notification.tf
+++ examples/notification.tf
@@ -1,54 +1,48 @@
 
-resource "sysdig_secure_notification_channel" "sample-email" {
+resource "sysdig_secure_notification_channel_email" "sample-email" {
   name                 = "Example Channel - Email"
   enabled              = true
-  type                 = "EMAIL"
-  recipients           = "root@localhost.com"
+  recipients           = ["root@localhost.com"]
   notify_when_ok       = false
   notify_when_resolved = false
 }
 
-resource "sysdig_secure_notification_channel" "sample-amazon-sns" {
+resource "sysdig_secure_notification_channel_sns" "sample-amazon-sns" {
   name                 = "Example Channel - Amazon SNS"
   enabled              = true
-  type                 = "SNS"
-  topics               = "arn:aws:sns:us-east-1:273107874544:my-alerts,arn:aws:sns:us-east-1:273107874544:my-alerts2"
+  topics               = ["arn:aws:sns:us-east-1:273107874544:my-alerts", "arn:aws:sns:us-east-1:273107874544:my-alerts2"]
   notify_when_ok       = false
   notify_when_resolved = false
 }
 
-resource "sysdig_secure_notification_channel" "sample-victorops" {
+resource "sysdig_secure_notification_channel_victorops" "sample-victorops" {
   name                 = "Example Channel - VictorOps"
   enabled              = true
-  type                 = "VICTOROPS"
   api_key              = "1234342-4234243-4234-2"
   routing_key          = "My team"
   notify_when_ok       = false
   notify_when_resolved = false
 }
 
-resource "sysdig_secure_notification_channel" "sample-opsgenie" {
+resource "sysdig_secure_notification_channel_opsgenie" "sample-opsgenie" {
   name                 = "Example Channel - OpsGenie"
   enabled              = true
-  type                 = "OPSGENIE"
   api_key              = "2349324-342354353-5324-23"
   notify_when_ok       = false
   notify_when_resolved = false
 }
 
-resource "sysdig_secure_notification_channel" "sample-webhook" {
+resource "sysdig_secure_notification_channel_webhook" "sample-webhook" {
   name                 = "Example Channel - Webhook"
   enabled              = true
-  type                 = "WEBHOOK"
   url                  = "localhost:8080"
   notify_when_ok       = false
   notify_when_resolved = false
 }
 
-resource "sysdig_secure_notification_channel" "sample-slack" {
+resource "sysdig_secure_notification_channel_slack" "sample-slack" {
   name                 = "Example Channel - Slack"
   enabled              = true
-  type                 = "SLACK"
   url                  = "https://hooks.slack.cwom/services/XXXXXXXXX/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX"
   channel              = "#sysdig"
   notify_when_ok       = true
--- examples/user.tf
+++ examples/user.tf
@@ -1,6 +1,6 @@
 resource "sysdig_user" "sample" {
-  email      = "sample@example.com"
+  email       = "sample@example.com"
   system_role = "ROLE_CUSTOMER"
-  first_name = "John"
-  last_name  = "Smith"
+  first_name  = "John"
+  last_name   = "Smith"
 }
