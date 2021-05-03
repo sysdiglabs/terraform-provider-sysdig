7b869569144c3d58c96d2687608fa91653da0945
Federico Barcelona <fede_rico_94@hotmail.com>
2021-05-03 16:31:22 +0200

    fix(ci): Solve problems with webhooks

--- sysdig/resource_sysdig_secure_notification_channel_webhook_test.go
+++ sysdig/resource_sysdig_secure_notification_channel_webhook_test.go
@@ -45,7 +45,7 @@ func secureNotificationChannelWebhookWithName(name string) string {
 resource "sysdig_secure_notification_channel_webhook" "sample-webhook" {
 	name = "Example Channel %s - Webhook"
 	enabled = true
-	url = "localhost:8080"
+	url = "http://1.1.1.1:8080"
 	notify_when_ok = false
 	notify_when_resolved = false
 	send_test_notification = false
