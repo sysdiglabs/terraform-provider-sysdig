036d69accb85bd450d10bfc30f34e04deab15fc9
Noah Kraemer <58708692+nkraemer-sysdig@users.noreply.github.com>
2021-08-06 04:46:44 -0700

    feat: Add secure CloudAccount resource (#111)
    * add secure cloudAccount resource
    
    * use correct json field
    
    * correct query param, mark field as computed
    
    * clear ID on read error

--- go.mod
+++ go.mod
@@ -6,13 +6,10 @@ require (
 	cloud.google.com/go v0.76.0 // indirect
 	cloud.google.com/go/storage v1.13.0 // indirect
 	github.com/agext/levenshtein v1.2.3 // indirect
-	github.com/agl/ed25519 v0.0.0-20170116200512-5312a6153412 // indirect
-	github.com/alcortesm/tgz v0.0.0-20161220082320-9c5fe88206d7 // indirect
 	github.com/apparentlymart/go-cidr v1.1.0 // indirect
 	github.com/aws/aws-sdk-go v1.37.10 // indirect
 	github.com/fatih/color v1.12.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-cty v1.4.1-0.20200723130312-85980079f637
 	github.com/hashicorp/go-hclog v0.16.2 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
@@ -24,7 +21,6 @@ require (
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.7.0
 	github.com/hashicorp/yamux v0.0.0-20210707203944-259a57b3608c // indirect
 	github.com/jmespath/go-jmespath v0.4.0
-	github.com/keybase/go-crypto v0.0.0-20161004153544-93f5b35093ba // indirect
 	github.com/klauspost/compress v1.11.7 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
---
+++ sysdig/internal/client/secure/cloud_account.go
@@ -0,0 +1,86 @@
+package secure
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func (client *sysdigSecureClient) cloudAccountURL(includeExternalID bool) string {
+	if includeExternalID {
+		return fmt.Sprintf("%s/api/cloud/v2/accounts?includeExternalID=true", client.URL)
+	}
+	return fmt.Sprintf("%s/api/cloud/v2/accounts", client.URL)
+}
+
+func (client *sysdigSecureClient) cloudAccountByIdURL(accountID string, includeExternalID bool) string {
+	if includeExternalID {
+		return fmt.Sprintf("%s/api/cloud/v2/accounts/%s?includeExternalID=true", client.URL, accountID)
+	}
+	return fmt.Sprintf("%s/api/cloud/v2/accounts/%s", client.URL, accountID)
+}
+
+func (client *sysdigSecureClient) CreateCloudAccount(ctx context.Context, cloudAccount *CloudAccount) (*CloudAccount, error) {
+	response, err := client.doSysdigSecureRequest(ctx, http.MethodPost, client.cloudAccountURL(true), cloudAccount.ToJSON())
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK && response.StatusCode != http.StatusCreated {
+		err = errorFromResponse(response)
+		return nil, err
+	}
+
+	bodyBytes, _ := ioutil.ReadAll(response.Body)
+	return CloudAccountFromJSON(bodyBytes), nil
+}
+
+func (client *sysdigSecureClient) GetCloudAccountById(ctx context.Context, accountID string) (*CloudAccount, error) {
+	response, err := client.doSysdigSecureRequest(ctx, http.MethodGet, client.cloudAccountByIdURL(accountID, true), nil)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, errorFromResponse(response)
+	}
+
+	bodyBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return CloudAccountFromJSON(bodyBytes), nil
+}
+
+func (client *sysdigSecureClient) DeleteCloudAccount(ctx context.Context, accountID string) error {
+	response, err := client.doSysdigSecureRequest(ctx, http.MethodDelete, client.cloudAccountByIdURL(accountID, false), nil)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusNoContent && response.StatusCode != http.StatusOK {
+		return errorFromResponse(response)
+	}
+	return nil
+}
+
+func (client *sysdigSecureClient) UpdateCloudAccount(ctx context.Context, accountID string, cloudAccount *CloudAccount) (*CloudAccount, error) {
+	response, err := client.doSysdigSecureRequest(ctx, http.MethodPut, client.cloudAccountByIdURL(accountID, true), cloudAccount.ToJSON())
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		err = errorFromResponse(response)
+		return nil, err
+	}
+
+	bodyBytes, _ := ioutil.ReadAll(response.Body)
+	return CloudAccountFromJSON(bodyBytes), nil
+}
