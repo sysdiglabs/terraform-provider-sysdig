fd9628d35e61f225641df1858e1ca2905d2a5f5a
Diego Bonfigli <diego.bonfigli@sysdig.com>
2021-09-29 15:55:39 +0200

    feat: Support for PromQL Alerts (#108)

---
+++ sysdig/resource_sysdig_monitor_alert_promql_test.go
@@ -0,0 +1,56 @@
+package sysdig_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/draios/terraform-provider-sysdig/sysdig"
+)
+
+func TestAccAlertPromql(t *testing.T) {
+	rText := func() string { return acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum) }
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			if v := os.Getenv("SYSDIG_MONITOR_API_TOKEN"); v == "" {
+				t.Fatal("SYSDIG_MONITOR_API_TOKEN must be set for acceptance tests")
+			}
+		},
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"sysdig": func() (*schema.Provider, error) {
+				return sysdig.Provider(), nil
+			},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: alertPromqlWithName(rText()),
+			},
+			{
+				ResourceName:      "sysdig_monitor_alert_promql.sample",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func alertPromqlWithName(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_monitor_alert_promql" "sample" {
+	name = "TERRAFORM TEST - PROMQL %s"
+	description = "TERRAFORM TEST - PROMQL %s"
+	severity = 3
+
+	promql = "(elasticsearch_jvm_memory_used_bytes{area=\"heap\"} / elasticsearch_jvm_memory_max_bytes{area=\"heap\"}) * 100 > 80"
+
+	trigger_after_minutes = 10
+
+	enabled = false
+}
+`, name, name)
+}
