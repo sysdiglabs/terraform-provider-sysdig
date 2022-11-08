c56e34955fc63492f0dcc262a05827a9387201ff
arturodilecce <84313881+arturodilecce@users.noreply.github.com>
2022-11-08 13:49:30 +0100

    Feat: support for alertV2 (#194)
    * AlertV2 model and client methods, AlertV2Prometheus resource
    
    * fix linting
    
    * COEOWNERS for monitor
    
    * fix notification_channels.type to be required
    
    * update AlertV2Prometheus docs

---
+++ sysdig/resource_sysdig_monitor_alert_v2_prometheus.go
@@ -0,0 +1,162 @@
+package sysdig
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/draios/terraform-provider-sysdig/sysdig/internal/client/monitor"
+)
+
+func resourceSysdigMonitorAlertV2Prometheus() *schema.Resource {
+
+	timeout := 5 * time.Minute
+
+	return &schema.Resource{
+		CreateContext: resourceSysdigMonitorAlertV2PrometheusCreate,
+		UpdateContext: resourceSysdigMonitorAlertV2PrometheusUpdate,
+		ReadContext:   resourceSysdigMonitorAlertV2PrometheusRead,
+		DeleteContext: resourceSysdigMonitorAlertV2PrometheusDelete,
+		// CustomizeDiff: resourceSysdigMonitorAlertV2PrometheusCustomizeDiff,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(timeout),
+			Update: schema.DefaultTimeout(timeout),
+			Read:   schema.DefaultTimeout(timeout),
+			Delete: schema.DefaultTimeout(timeout),
+		},
+
+		Schema: createAlertV2Schema(map[string]*schema.Schema{
+			"query": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		}),
+	}
+}
+
+func resourceSysdigMonitorAlertV2PrometheusCreate(ctx context.Context, d *schema.ResourceData, i interface{}) diag.Diagnostics {
+	client, err := i.(SysdigClients).sysdigMonitorClient()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	a := buildAlertV2PrometheusStruct(d)
+
+	aCreated, err := client.CreateAlertV2Prometheus(ctx, *a)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(strconv.Itoa(aCreated.ID))
+
+	err = updateAlertV2PrometheusState(d, &aCreated)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func resourceSysdigMonitorAlertV2PrometheusRead(ctx context.Context, d *schema.ResourceData, i interface{}) diag.Diagnostics {
+	client, err := i.(SysdigClients).sysdigMonitorClient()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	id, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	a, err := client.GetAlertV2PrometheusById(ctx, id)
+
+	if err != nil {
+		d.SetId("")
+		return nil
+	}
+
+	err = updateAlertV2PrometheusState(d, &a)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func resourceSysdigMonitorAlertV2PrometheusUpdate(ctx context.Context, d *schema.ResourceData, i interface{}) diag.Diagnostics {
+	client, err := i.(SysdigClients).sysdigMonitorClient()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	a := buildAlertV2PrometheusStruct(d)
+
+	a.ID, _ = strconv.Atoi(d.Id())
+
+	aUpdated, err := client.UpdateAlertV2Prometheus(ctx, *a)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = updateAlertV2PrometheusState(d, &aUpdated)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func resourceSysdigMonitorAlertV2PrometheusDelete(ctx context.Context, d *schema.ResourceData, i interface{}) diag.Diagnostics {
+	client, err := i.(SysdigClients).sysdigMonitorClient()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	id, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = client.DeleteAlertV2Prometheus(ctx, id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+// func resourceSysdigMonitorAlertV2PrometheusCustomizeDiff(ctx context.Context, diff *schema.ResourceDiff, i interface{}) error {
+// 	return nil
+// }
+
+func buildAlertV2PrometheusStruct(d *schema.ResourceData) (alert *monitor.AlertV2Prometheus) {
+	alertV2Common := buildAlertV2CommonStruct(d)
+	alertV2Common.Type = monitor.AlertV2AlertType_Prometheus
+
+	config := &monitor.AlertV2ConfigPrometheus{}
+	config.Query = d.Get("query").(string)
+
+	alert = &monitor.AlertV2Prometheus{
+		AlertV2Common: *alertV2Common,
+		Config:        config,
+	}
+	return
+}
+
+func updateAlertV2PrometheusState(d *schema.ResourceData, alert *monitor.AlertV2Prometheus) (err error) {
+	err = updateAlertV2CommonState(d, &alert.AlertV2Common)
+	if err != nil {
+		return
+	}
+
+	_ = d.Set("query", alert.Config.Query)
+
+	return
+}
---
+++ website/docs/r/monitor_alert_v2_prometheus.md
@@ -0,0 +1,90 @@
+---
+subcategory: "Sysdig Monitor"
+layout: "sysdig"
+page_title: "Sysdig: sysdig_monitor_alert_v2_prometheus"
+description: |-
+  Creates a Sysdig Monitor PromQL Alert with AlertV2 API.
+---
+
+# Resource: sysdig_monitor_alert_v2_prometheus
+
+Creates a Sysdig Monitor Prometheus Alert. The notification is triggered on the user-defined PromQL expression.
+
+-> **Note:** Sysdig Terraform Provider is under rapid development at this point. If you experience any issue or discrepancy while using it, please make sure you have the latest version. If the issue persists, or you have a Feature Request to support an additional set of resources, please open a [new issue](https://github.com/sysdiglabs/terraform-provider-sysdig/issues/new) in the GitHub repository.
+
+## Example Usage
+
+```terraform
+resource "sysdig_monitor_alert_v2_prometheus" "sample" {
+	name = "Elasticsearch JVM heap usage"
+	description = "A Kubernetes pod failed to restart"
+	severity = high
+	query = "(elasticsearch_jvm_memory_used_bytes{area=\"heap\"} / elasticsearch_jvm_memory_max_bytes{area=\"heap\"}) * 100 > 80"
+	trigger_after_minutes = 10
+	notification_channels {
+		id = <your-notification-channel-id>
+    type = "EMAIL""
+    renotify_every_minutes = 5
+	}
+}
+```
+
+## Argument Reference
+
+### Common alert arguments
+
+These arguments are common to all alerts in Sysdig Monitor.
+
+* `name` - (Required) The name of the Monitor alert. It must be unique.
+* `description` - (Optional) The description of Monitor alert.
+* `trigger_after_minutes` - (Required) Threshold of time for the status to stabilize until the alert is fired.
+* `group` - (Optional) Lowercase string to group alerts in the UI
+* `severity` - (Optional) Severity of the Monitor alert. It must be high, medium, low or info
+* `enabled` - (Optional) Boolean that defines if the alert is enabled or not. Defaults to true.
+* `notification_channels` - (Optional) List of notification channel configuration
+* `custom_notification` - (Optional) Allows to define a custom notification title, prepend and append text.
+* `capture` - (Optional) Allows to define a configuration to trigger a Sysdig Capture.
+
+### `notification_channels` - 
+
+By defining this field, the user can choose to which notification channels send the events when the alert fires. 
+
+It is a list of objects with the following fields:
+* `id` - (Required) The ID of the notification channel
+* `type` - (Required) The type of the notification channel
+* `renotify_every_minutes`: (Optional) the amount of minutes to wait before re sending the notification to this channel
+
+### `custom_notification`
+
+By defining this field, the user can modify the title and the body of the message sent when the alert
+is fired.
+
+* `subject` - (Required) Sets the title of the alert. It is commonly defined as `{{__alert_name__}} is {{__alert_status__}}`.
+* `prepend` - (Optional) Text to add before the alert template.
+* `append` - (Optional) Text to add after the alert template.
+
+### Prometheus alert arguments
+
+* `query` - (Required) PromQL-based metric expression to alert on. Example: `histogram_quantile(0.99, rate(etcd_http_successful_duration_seconds_bucket[5m]) > 0.15` or `predict_linear(sysdig_fs_free_bytes{fstype!~"tmpfs"}[1h], 24*3600) < 10000000000`.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+### Common alert attributes
+
+In addition to all arguments above, the following attributes are exported, which are common to all the
+alerts in Sysdig Monitor:
+
+* `id` - ID of the alert created.
+* `version` - Current version of the resource in Sysdig Monitor.
+* `team` - Team ID that owns the alert.
+
+
+## Import
+
+Prometheus Monitor alerts can be imported using the alert ID, e.g.
+
+```
+$ terraform import sysdig_monitor_alert_v2_prometheus.example 12345
+```
