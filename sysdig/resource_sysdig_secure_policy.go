78649f54abeffc0640351398252abc1f08bec8e8
Rick Badertscher <rick.badertscher@gmail.com>
2022-11-08 07:48:30 -0800

    go fmt

--- sysdig/resource_sysdig_secure_policy.go
+++ sysdig/resource_sysdig_secure_policy.go
@@ -233,20 +233,20 @@ func addActionsToPolicy(d *schema.ResourceData, policy *secure.Policy) {
 		beforeEventNs := d.Get("actions.0.capture.0.seconds_before_event").(int) * 1000000000
 		name := d.Get("actions.0.capture.0.name").(string)
 		if len(name) <= 0 {
-                        policy.Actions = append(policy.Actions, secure.Action{
-                                Type:                 "POLICY_ACTION_CAPTURE",
-                                IsLimitedToContainer: false,
-                                AfterEventNs:         afterEventNs,
-                                BeforeEventNs:        beforeEventNs,
-                        })
+			policy.Actions = append(policy.Actions, secure.Action{
+				Type:                 "POLICY_ACTION_CAPTURE",
+				IsLimitedToContainer: false,
+				AfterEventNs:         afterEventNs,
+				BeforeEventNs:        beforeEventNs,
+			})
 		} else {
-                        policy.Actions = append(policy.Actions, secure.Action{
-                                Type:                 "POLICY_ACTION_CAPTURE",
-                                IsLimitedToContainer: false,
-                                AfterEventNs:         afterEventNs,
-                                BeforeEventNs:        beforeEventNs,
-        			Name:                 name,
-                        })
+			policy.Actions = append(policy.Actions, secure.Action{
+				Type:                 "POLICY_ACTION_CAPTURE",
+				IsLimitedToContainer: false,
+				AfterEventNs:         afterEventNs,
+				BeforeEventNs:        beforeEventNs,
+				Name:                 name,
+			})
 		}
 	}
 }
