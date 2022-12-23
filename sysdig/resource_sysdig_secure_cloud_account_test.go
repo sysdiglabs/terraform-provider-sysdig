53c79ba9370e36345dd7724c2b559d3cc738af91
sameer-in <sameer.inamdar@sysdig.com>
2022-12-23 14:01:08 -0800

    minor edit

--- sysdig/resource_sysdig_secure_cloud_account_test.go
+++ sysdig/resource_sysdig_secure_cloud_account_test.go
@@ -81,8 +81,6 @@ resource "sysdig_secure_cloud_account" "sample-3" {
   account_id          = "sample3-%s"
   cloud_provider      = "gcp"
   alias               = "%s"
-  role_enabled        = "false"
-  role_name            = "CustomRoleName"
   workload_identity_account_id = "sample3-%s"
 }
 `, accountID, accountID, accountID)
