ec766d4c6c2fdfe7584e33868de2f29d45c48c69
sameer-in <sameer.inamdar@sysdig.com>
2023-01-10 15:50:30 -0800

    fixing test

--- sysdig/resource_sysdig_secure_cloud_account_test.go
+++ sysdig/resource_sysdig_secure_cloud_account_test.go
@@ -100,5 +100,5 @@ resource "sysdig_secure_cloud_account" "sample-1" {
   workload_identity_account_id = "sample-1-%s"
   workload_identity_account_alias = "%s"
 }
-`, accountID, accountID, accountID)
+`, accountID, accountID, accountID, accountID)
 }
