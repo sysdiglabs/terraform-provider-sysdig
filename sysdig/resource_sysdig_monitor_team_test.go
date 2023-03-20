d9628d005ac05800c0d9f084c8944dee17a57c71
Filip Tubic <tubefilip@gmail.com>
2023-03-20 18:29:08 +0100

    refactor(tests): mark team tests as ibm tests

--- sysdig/resource_sysdig_monitor_team_test.go
+++ sysdig/resource_sysdig_monitor_team_test.go
@@ -1,10 +1,9 @@
-//go:build tf_acc_sysdig
+//go:build tf_acc_sysdig || tf_acc_ibm
 
 package sysdig_test
 
 import (
 	"fmt"
-	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
@@ -18,11 +17,11 @@ func TestAccMonitorTeam(t *testing.T) {
 	rText := func() string { return acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum) }
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: func() {
-			if v := os.Getenv("SYSDIG_MONITOR_API_TOKEN"); v == "" {
-				t.Fatal("SYSDIG_MONITOR_API_TOKEN must be set for acceptance tests")
-			}
-		},
+		//PreCheck: func() {
+		//	if v := os.Getenv("SYSDIG_MONITOR_API_TOKEN"); v == "" {
+		//		t.Fatal("SYSDIG_MONITOR_API_TOKEN must be set for acceptance tests")
+		//	}
+		//},
 		ProviderFactories: map[string]func() (*schema.Provider, error){
 			"sysdig": func() (*schema.Provider, error) {
 				return sysdig.Provider(), nil
