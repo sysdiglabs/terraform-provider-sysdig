74d83b262a53c8f1bc992dbd59305e026883920a
Filip Tubic <tubefilip@gmail.com>
2023-03-21 13:26:31 +0100

    refactor(tests): add ibm secure env vars

---
+++ sysdig/resource_sysdig_monitor_team_ibm_test.go
@@ -0,0 +1,43 @@
+//go:build tf_acc_ibm
+
+package sysdig_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/draios/terraform-provider-sysdig/sysdig"
+)
+
+func TestAccMonitorIBMTeam(t *testing.T) {
+	rText := func() string { return acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum) }
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			if v := os.Getenv("SYSDIG_IBM_MONITOR_API_KEY"); v == "" {
+				t.Fatal("SYSDIG_IBM_MONITOR_API_KEY must be set for acceptance tests")
+			}
+		},
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"sysdig": func() (*schema.Provider, error) {
+				return sysdig.Provider(), nil
+			},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: monitorTeamMinimumConfiguration(rText()),
+			},
+			{
+				Config: monitorTeamWithName(rText()),
+			},
+			{
+				ResourceName:      "sysdig_monitor_team.sample",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
