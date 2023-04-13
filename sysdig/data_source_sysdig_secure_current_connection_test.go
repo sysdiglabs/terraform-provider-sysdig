b2d6e1cf6466f6746adbd7e933e629b1269ee0ef
Julio J <papajulio21@gmail.com>
2023-04-13 19:08:07 +0200

    Fix test when being run against staging env instead of prod

--- sysdig/data_source_sysdig_secure_current_connection_test.go
+++ sysdig/data_source_sysdig_secure_current_connection_test.go
@@ -17,6 +17,7 @@ func TestAccSecureConnection(t *testing.T) {
 	dataSourceResourceName := "data.sysdig_secure_connection.current"
 
 	apiToken := os.Getenv("SYSDIG_SECURE_API_TOKEN")
+    secureUrl := os.Getenv("SYSDIG_SECURE_URL")
 
 	resource.ParallelTest(t, resource.TestCase{
 
@@ -34,7 +35,7 @@ func TestAccSecureConnection(t *testing.T) {
 			{
 				Config: getSysdigSecureCurrentConnection(),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(dataSourceResourceName, "secure_url", "https://secure.sysdig.com"),
+					resource.TestCheckResourceAttr(dataSourceResourceName, "secure_url", secureUrl),
 					resource.TestCheckResourceAttr(dataSourceResourceName, "secure_api_token", apiToken),
 				),
 			},
