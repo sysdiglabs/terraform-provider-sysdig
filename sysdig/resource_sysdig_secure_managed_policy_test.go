a57b2a618bb4dd582b01908fe6c77fb579bd813c
Ben Lucas <ben.lucas@sysdig.com>
2023-05-05 11:13:17 -0700

    fix broken tests

--- sysdig/resource_sysdig_secure_managed_policy_test.go
+++ sysdig/resource_sysdig_secure_managed_policy_test.go
@@ -2,9 +2,19 @@
 
 package sysdig_test
 
-func TestAccManagedPolicy(t *testing.T) {
-	rText := func() string { return acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum) }
+import (
+	"fmt"
+	"os"
+	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/draios/terraform-provider-sysdig/sysdig"
+)
+
+func TestAccManagedPolicy(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			if v := os.Getenv("SYSDIG_SECURE_API_TOKEN"); v == "" {
