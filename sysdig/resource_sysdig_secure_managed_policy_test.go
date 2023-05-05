c6c21f268ec3b02ea149f943b9d8bde533bb3409
Ben Lucas <ben.lucas@sysdig.com>
2023-05-05 11:22:03 -0700

    fix broken tests

--- sysdig/resource_sysdig_secure_managed_policy_test.go
+++ sysdig/resource_sysdig_secure_managed_policy_test.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
