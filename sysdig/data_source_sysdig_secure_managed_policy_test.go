a763bde23999e626d49843a9e226ea2a26eaf98f
Ben Lucas <ben.lucas@sysdig.com>
2023-05-12 12:07:00 -0700

    fix broken tests

--- sysdig/data_source_sysdig_secure_managed_policy_test.go
+++ sysdig/data_source_sysdig_secure_managed_policy_test.go
@@ -2,6 +2,16 @@
 
 package sysdig_test
 
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/draios/terraform-provider-sysdig/sysdig"
+)
+
 func TestAccManagedPolicyDataSource(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
