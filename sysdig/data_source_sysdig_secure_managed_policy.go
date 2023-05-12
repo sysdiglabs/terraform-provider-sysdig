f051da98aff2a89955a792ed0d57a56872307759
Ben Lucas <ben.lucas@sysdig.com>
2023-05-12 12:55:35 -0700

    reuse function for getting managed policy

--- sysdig/data_source_sysdig_secure_managed_policy.go
+++ sysdig/data_source_sysdig_secure_managed_policy.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"time"
 
-	v2 "github.com/draios/terraform-provider-sysdig/sysdig/internal/client/v2"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -29,27 +28,10 @@ func dataSourceSysdigManagedPolicyRead(ctx context.Context, d *schema.ResourceDa
 		return diag.FromErr(err)
 	}
 
-	policies, _, err := client.GetPolicies(ctx)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
 	policyName := d.Get("name").(string)
 	policyType := d.Get("type").(string)
 
-	var policy v2.Policy
-	for _, existingPolicy := range policies {
-		if existingPolicy.Name == policyName && existingPolicy.Type == policyType {
-			if !existingPolicy.IsDefault {
-				return diag.Errorf("policy is not a managed policy - use `resource_sysdig_secure_policy`")
-			}
-			policy = existingPolicy
-		}
-	}
-
-	if policy.ID == 0 {
-		return diag.Errorf("unable to find managed policy")
-	}
+	policy, err := getManagedPolicy(ctx, client, policyName, policyType)
 
 	loadedPolicy, _, err := client.GetPolicyByID(ctx, policy.ID)
 	if err != nil {
