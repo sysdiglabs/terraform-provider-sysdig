6f524c8923d235755ed4fc46c9cefe1e64a283cb
Ben Lucas <ben.lucas@sysdig.com>
2023-05-15 15:10:15 -0700

    add tests for custom policy resource

---
+++ sysdig/resource_sysdig_secure_custom_policy_test.go
@@ -0,0 +1,260 @@
+//go:build tf_acc_sysdig || tf_acc_sysdig_secure || tf_acc_policies
+
+package sysdig_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/draios/terraform-provider-sysdig/sysdig"
+)
+
+func TestAccCustomPolicy(t *testing.T) {
+	rText := func() string { return acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum) }
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: preCheckAnyEnv(t, SysdigSecureApiTokenEnv),
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"sysdig": func() (*schema.Provider, error) {
+				return sysdig.Provider(), nil
+			},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: policyWithName(rText()),
+			},
+			{
+				ResourceName:      "sysdig_secure_custom_policy.sample",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: policyWithoutActions(rText()),
+			},
+			{
+				Config: policyWithoutNotificationChannels(rText()),
+			},
+			{
+				Config: policyWithMinimumConfiguration(rText()),
+			},
+			{
+				Config: policiesWithDifferentSeverities(rText()),
+			},
+			{
+				Config: policiesWithKillAction(rText()),
+			},
+			{
+				Config: policiesForAWSCloudtrail(rText()),
+			},
+			{
+				Config: policiesForGCPAuditLog(rText()),
+			},
+			{
+				Config: policiesForAzurePlatformlogs(rText()),
+			},
+			{
+				Config: policiesWithDisabledRules(rText()),
+			}
+		},
+	})
+}
+
+func policyWithName(name string) string {
+	return fmt.Sprintf(`
+%s
+%s
+resource "sysdig_secure_custom_policy" "sample" {
+  name = "TERRAFORM TEST 1 %s"
+  description = "TERRAFORM TEST %s"
+  enabled = true
+  severity = 4
+  scope = "container.id != \"\""
+  runbook = "https://sysdig.com"
+
+  rules {
+	name = sysdig_secure_rule_falco.terminal_shell.name
+	enabled = true
+  }
+
+  actions {
+    container = "stop"
+    capture {
+      seconds_before_event = 5
+      seconds_after_event = 10
+      name = "testcapture"
+    }
+  }
+
+  notification_channels = [sysdig_secure_notification_channel_email.sample_email.id]
+}
+`, secureNotificationChannelEmailWithName(name), ruleFalcoTerminalShell(name), name, name)
+}
+
+func policyWithoutActions(name string) string {
+	return fmt.Sprintf(`
+%s
+%s
+resource "sysdig_secure_custom_policy" "sample2" {
+  name = "TERRAFORM TEST 2 %s"
+  description = "TERRAFORM TEST %s"
+  enabled = true
+  severity = 4
+  scope = "container.id != \"\""
+
+  notification_channels = [sysdig_secure_notification_channel_email.sample_email.id]
+
+  rules {
+	name = sysdig_secure_rule_falco.terminal_shell.name
+	enabled = true
+  }
+
+  actions {}
+}
+`, secureNotificationChannelEmailWithName(name), ruleFalcoTerminalShell(name), name, name)
+}
+
+func policyWithoutNotificationChannels(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_custom_policy" "sample3" {
+  name = "TERRAFORM TEST 3 %s"
+  description = "TERRAFORM TEST %s"
+  enabled = true
+  severity = 4
+  scope = "container.id != \"\""
+
+  rules {
+	name = sysdig_secure_rule_falco.terminal_shell.name
+	enabled = true
+  }
+  actions {}
+}
+`, name, name)
+}
+
+func policyWithMinimumConfiguration(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_custom_policy" "sample4" {
+  name = "TERRAFORM TEST 4 %s"
+  description = "TERRAFORM TEST %s"
+  actions {}
+}
+`, name, name)
+}
+
+func policiesWithDifferentSeverities(name string) (res string) {
+	for i := 0; i <= 7; i++ {
+		res += fmt.Sprintf(`
+resource "sysdig_secure_custom_policy" "sample_%d" {
+  name = "TERRAFORM TEST 1 %s-%d"
+  description = "TERRAFORM TEST %s-%d"
+  enabled = true
+  severity = %d
+  scope = "container.id != \"\""
+  rules {
+	name = "Terminal shell in container"
+	enabled = true
+  }
+
+  actions {
+    container = "stop"
+    capture {
+      seconds_before_event = 5
+      seconds_after_event = 10
+      name = "capture_name"
+    }
+  }
+}
+
+`, i, name, i, name, i, i)
+	}
+	return
+}
+
+func policiesWithKillAction(name string) (res string) {
+	return fmt.Sprintf(`
+resource "sysdig_secure_custom_policy" "sample" {
+  name = "TERRAFORM TEST 1 %s"
+  description = "TERRAFORM TEST %s"
+  enabled = true
+  severity = 4
+  scope = "container.id != \"\""
+
+  rules {
+	name = "Terminal shell in container"
+	enabled = true
+  }
+
+  actions {
+    container = "kill"
+  }
+}
+`, name, name)
+}
+
+func policiesForAWSCloudtrail(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_custom_policy" "sample4" {
+  name = "TERRAFORM TEST 4 %s"
+  description = "TERRAFORM TEST %s"
+  type = "aws_cloudtrail"
+  actions {}
+}
+`, name, name)
+}
+
+func policiesForGCPAuditLog(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_custom_policy" "sample5" {
+  name = "TERRAFORM TEST %s"
+  description = "TERRAFORM TEST %s"
+  type = "gcp_auditlog"
+  actions {}
+}
+`, name, name)
+}
+
+func policiesForAzurePlatformlogs(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_custom_policy" "sample6" {
+  name = "TERRAFORM TEST %s"
+  description = "TERRAFORM TEST %s"
+  type = "azure_platformlogs"
+  actions {}
+}
+`, name, name)
+}
+
+func policiesWithDisabledRules(name string) string {
+	return fmt.Sprintf(`
+%s
+%s
+resource "sysdig_secure_custom_policy" "sample" {
+  name = "TERRAFORM TEST 1 %s"
+  description = "TERRAFORM TEST %s"
+  enabled = true
+  severity = 4
+  scope = "container.id != \"\""
+  runbook = "https://sysdig.com"
+
+  rules {
+	name = sysdig_secure_rule_falco.terminal_shell.name
+	enabled = false
+  }
+
+  actions {
+    container = "stop"
+    capture {
+      seconds_before_event = 5
+      seconds_after_event = 10
+      name = "testcapture"
+    }
+  }
+
+  notification_channels = [sysdig_secure_notification_channel_email.sample_email.id]
+}
+`, secureNotificationChannelEmailWithName(name), ruleFalcoTerminalShell(name), name, name)
+}
