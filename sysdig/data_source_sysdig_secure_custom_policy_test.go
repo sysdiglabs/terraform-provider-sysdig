0d5ba10eca161dff257689c4259407cc7300c60d
Ben Lucas <ben.lucas@sysdig.com>
2023-05-16 11:25:01 -0700

    fix broken tests

--- sysdig/data_source_sysdig_secure_custom_policy_test.go
+++ sysdig/data_source_sysdig_secure_custom_policy_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
