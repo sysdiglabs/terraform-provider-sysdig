94fee01a613d7d649774cadc35ada5b0b1a08072
Filip Tubic <tubefilip@gmail.com>
2023-05-19 11:57:28 +0200

    feat(tests): remove tf_acc_sysdig and tf_acc_ibm tags

--- sysdig/data_source_sysdig_current_user_test.go
+++ sysdig/data_source_sysdig_current_user_test.go
@@ -1,4 +1,4 @@
-//go:build tf_acc_sysdig || tf_acc_sysdig_monitor || tf_acc_sysdig_secure || tf_acc_ibm || tf_acc_ibm_monitor
+//go:build tf_acc_sysdig_monitor || tf_acc_sysdig_secure || tf_acc_ibm_monitor
 
 package sysdig_test
 
