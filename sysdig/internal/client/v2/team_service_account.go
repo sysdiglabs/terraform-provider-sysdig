ab4e48e9d7e37e06fe56eea58ef8da6936de7de2
lombard <emanuele.lombardi@sysdig.com>
2023-07-20 16:05:42 +0200

    add support for team service account

---
+++ sysdig/internal/client/v2/team_service_account.go
@@ -0,0 +1,123 @@
+package v2
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+var TeamServiceAccountNotFound = errors.New("group mapping not found")
+
+const (
+	ServiceAccountsPath      = "%s/api/serviceaccounts/team"
+	ServiceAccountPath       = "%s/api/serviceaccounts/team/%d"
+	ServiceAccountDeletePath = "%s/api/serviceaccounts/team/%d/delete"
+)
+
+type TeamServiceAccountInterface interface {
+	Base
+	GetTeamServiceAccountById(ctx context.Context, id int) (*TeamServiceAccount, error)
+	CreateTeamServiceAccount(ctx context.Context, account *TeamServiceAccount) (*TeamServiceAccount, error)
+	UpdateTeamServiceAccount(ctx context.Context, account *TeamServiceAccount, id int) (*TeamServiceAccount, error)
+	DeleteTeamServiceAccount(ctx context.Context, id int) error
+}
+
+func (client *Client) GetTeamServiceAccountById(ctx context.Context, id int) (*TeamServiceAccount, error) {
+	response, err := client.requester.Request(ctx, http.MethodGet, client.GetTeamServiceAccountURL(id), nil)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		if response.StatusCode == http.StatusNotFound {
+			return nil, TeamServiceAccountNotFound
+		}
+		return nil, client.ErrorFromResponse(response)
+	}
+
+	teamServiceaccount, err := Unmarshal[TeamServiceAccount](response.Body)
+	if err != nil {
+		return nil, err
+	}
+	return &teamServiceaccount, nil
+}
+
+func (client *Client) CreateTeamServiceAccount(ctx context.Context, account *TeamServiceAccount) (*TeamServiceAccount, error) {
+	payload, err := Marshal(account)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := client.requester.Request(ctx, http.MethodPost, client.CreateTeamServiceAccountURL(), payload)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, client.ErrorFromResponse(response)
+	}
+
+	created, err := Unmarshal[TeamServiceAccount](response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &created, nil
+}
+
+func (client *Client) UpdateTeamServiceAccount(ctx context.Context, account *TeamServiceAccount, id int) (*TeamServiceAccount, error) {
+	payload, err := Marshal(account)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := client.requester.Request(ctx, http.MethodPut, client.UpdateTeamServiceAccountURL(id), payload)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, client.ErrorFromResponse(response)
+	}
+
+	updated, err := Unmarshal[TeamServiceAccount](response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &updated, nil
+}
+
+func (client *Client) DeleteTeamServiceAccount(ctx context.Context, id int) error {
+	response, err := client.requester.Request(ctx, http.MethodDelete, client.DeleteTeamServiceAccountURL(id), nil)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusNoContent && response.StatusCode != http.StatusOK && response.StatusCode != http.StatusNotFound {
+		return client.ErrorFromResponse(response)
+	}
+
+	return nil
+}
+
+func (client *Client) GetTeamServiceAccountURL(id int) string {
+	return fmt.Sprintf(ServiceAccountPath, client.config.url, id)
+}
+
+func (client *Client) CreateTeamServiceAccountURL() string {
+	return fmt.Sprintf(ServiceAccountsPath, client.config.url)
+}
+
+func (client *Client) UpdateTeamServiceAccountURL(id int) string {
+	return fmt.Sprintf(ServiceAccountPath, client.config.url, id)
+}
+
+func (client *Client) DeleteTeamServiceAccountURL(id int) string {
+	return fmt.Sprintf(ServiceAccountDeletePath, client.config.url, id)
+}
---
+++ sysdig/resource_sysdig_team_service_account.go
@@ -0,0 +1,197 @@
+package sysdig
+
+import (
+	"context"
+	v2 "github.com/draios/terraform-provider-sysdig/sysdig/internal/client/v2"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"strconv"
+	"time"
+)
+
+func resourceSysdigTeamServiceAccount() *schema.Resource {
+	timeout := 5 * time.Minute
+	return &schema.Resource{
+		ReadContext:   resourceSysdigTeamServiceAccountRead,
+		CreateContext: resourceSysdigTeamServiceAccountCreate,
+		UpdateContext: resourceSysdigTeamServiceAccountUpdate,
+		DeleteContext: resourceSysdigTeamServiceAccountDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(timeout),
+			Update: schema.DefaultTimeout(timeout),
+			Read:   schema.DefaultTimeout(timeout),
+			Delete: schema.DefaultTimeout(timeout),
+		},
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"role": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "ROLE_TEAM_READ",
+			},
+			"expiration_date": {
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"team_id": {
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"system_role": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "ROLE_SERVICE_ACCOUNT",
+			},
+			"date_created": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"api_key": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSysdigTeamServiceAccountRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client, err := m.(SysdigClients).sysdigCommonClientV2()
+	if err != nil {
+		diag.FromErr(err)
+	}
+
+	id, err := strconv.Atoi(d.Id())
+	if err != nil {
+		diag.FromErr(err)
+	}
+
+	teamServiceAccount, err := client.GetTeamServiceAccountById(ctx, id)
+	if err != nil {
+		if err == v2.TeamServiceAccountNotFound {
+			d.SetId("")
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+
+	err = teamServiceAccountToResourceData(teamServiceAccount, d)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func resourceSysdigTeamServiceAccountCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var err error
+
+	client, err := m.(SysdigClients).sysdigCommonClientV2()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	teamServiceAccount := teamServiceAccountFromResourceData(d)
+	teamServiceAccount, err = client.CreateTeamServiceAccount(ctx, teamServiceAccount)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(strconv.Itoa(teamServiceAccount.ID))
+
+	resourceSysdigTeamServiceAccountRead(ctx, d, m)
+
+	return nil
+}
+
+func resourceSysdigTeamServiceAccountUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var err error
+
+	client, err := m.(SysdigClients).sysdigCommonClientV2()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	teamServiceAccount := teamServiceAccountFromResourceData(d)
+	id, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	teamServiceAccount.ID = id
+	_, err = client.UpdateTeamServiceAccount(ctx, teamServiceAccount, id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	resourceSysdigTeamServiceAccountRead(ctx, d, m)
+
+	return nil
+}
+
+func resourceSysdigTeamServiceAccountDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client, err := m.(SysdigClients).sysdigCommonClientV2()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	id, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	err = client.DeleteTeamServiceAccount(ctx, id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func teamServiceAccountFromResourceData(d *schema.ResourceData) *v2.TeamServiceAccount {
+	return &v2.TeamServiceAccount{
+		Name:           d.Get("name").(string),
+		TeamRole:       d.Get("role").(string),
+		ExpirationDate: int64(d.Get("expiration_date").(int) * 1000),
+		TeamId:         d.Get("team_id").(int),
+		SystemRole:     d.Get("system_role").(string),
+		ApiKey:         d.Get("api_key").(string),
+	}
+}
+
+func teamServiceAccountToResourceData(teamServiceAccount *v2.TeamServiceAccount, d *schema.ResourceData) error {
+	err := d.Set("name", teamServiceAccount.Name)
+	if err != nil {
+		return err
+	}
+	err = d.Set("role", teamServiceAccount.TeamRole)
+	if err != nil {
+		return err
+	}
+	err = d.Set("expiration_date", teamServiceAccount.ExpirationDate/1000)
+	if err != nil {
+		return err
+	}
+	err = d.Set("team_id", teamServiceAccount.TeamId)
+	if err != nil {
+		return err
+	}
+	err = d.Set("system_role", teamServiceAccount.SystemRole)
+	if err != nil {
+		return err
+	}
+	err = d.Set("date_created", teamServiceAccount.DateCreated)
+	if err != nil {
+		return err
+	}
+	err = d.Set("api_key", teamServiceAccount.ApiKey)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
