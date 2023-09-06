10f998d4a0b303e2e0ee3095a8ffe2d558fe5d9f
Raghav Anandan <raghav.anandan@raghav.anandan>
2023-09-06 10:11:34 -0700

    go fmt file

--- sysdig/resource_sysdig_secure_cloud_account_v2.go
+++ sysdig/resource_sysdig_secure_cloud_account_v2.go
@@ -24,43 +24,43 @@ func resourceSysdigSecureCloudAccountV2() *schema.Resource {
 		Timeouts: &schema.ResourceTimeout{
 			Create: schema.DefaultTimeout(timeout),
 			Update: schema.DefaultTimeout(timeout),
-			Read: schema.DefaultTimeout(timeout),
+			Read:   schema.DefaultTimeout(timeout),
 			Delete: schema.DefaultTimeout(timeout),
 		},
 		Schema: map[string]*schema.Schema{
 			"account_id": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Required: true,
 			},
 			"cloud_provider": {
-				Type: schema.TypeString,
-				Required: true,
+				Type:         schema.TypeString,
+				Required:     true,
 				ValidateFunc: validation.StringInSlice([]string{"gcp"}, false),
 			},
 			"alias": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Optional: true,
 			},
 			"role_enabled": {
-				Type: schema.TypeBool,
+				Type:     schema.TypeBool,
 				Optional: true,
-				Default: false,
+				Default:  false,
 			},
 			"role_name": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Optional: true,
-				Default: "SysdigCloudBench",
+				Default:  "SysdigCloudBench",
 			},
 			"external_id": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Computed: true,
 			},
 			"workload_identity_account_id": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Optional: true,
 			},
 			"workload_identity_account_alias": {
-				Type: schema.TypeString,
+				Type:     schema.TypeString,
 				Optional: true,
 			},
 		},
@@ -85,12 +85,12 @@ func resourceSysdigSecureCloudAccountV2Delete(ctx context.Context, d *schema.Res
 
 func cloudAccountFromResourceDataV2(d *schema.ResourceData) *v2.CloudAccountSecure {
 	return &v2.CloudAccountSecure{
-		AccountID: d.Get("account_id").(string),
-		Provider: d.Get("cloud_provider").(string),
-		Alias: d.Get("alias").(string),
-		RoleAvailable: d.Get("role_enabled").(bool),
-		RoleName: d.Get("role_name").(string),
-		WorkLoadIdentityAccountID: d.Get("workload_identity_account_id").(string),
+		AccountID:                    d.Get("account_id").(string),
+		Provider:                     d.Get("cloud_provider").(string),
+		Alias:                        d.Get("alias").(string),
+		RoleAvailable:                d.Get("role_enabled").(bool),
+		RoleName:                     d.Get("role_name").(string),
+		WorkLoadIdentityAccountID:    d.Get("workload_identity_account_id").(string),
 		WorkLoadIdentityAccountAlias: d.Get("workload_identity_account_alias").(string),
 	}
 }
