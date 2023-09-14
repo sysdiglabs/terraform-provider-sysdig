e7a8fbe9c71769ec7c39a2895b80cca95dbee856
Christopher Geers <christopher.geers@sysdig.com>
2023-09-13 22:01:10 -0500

    feat(cloudauth): features & components

--- sysdig/resource_sysdig_secure_cloud_auth_account.go
+++ sysdig/resource_sysdig_secure_cloud_auth_account.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func resourceSysdigSecureCloudauthAccount() *schema.Resource {
@@ -47,6 +48,11 @@ func resourceSysdigSecureCloudauthAccount() *schema.Resource {
 			"enabled": {
 				Type:     schema.TypeBool,
 				Required: true,
+			},
+			"feature": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem:     accountFeatures,
 			},
 			"components": {
 				Type:     schema.TypeList,
@@ -94,6 +100,49 @@ func resourceSysdigSecureCloudauthAccount() *schema.Resource {
 			},
 		},
 	}
+}
+
+var accountFeatures = &schema.Resource{
+	Schema: map[string]*schema.Schema{
+		"secureConfigPosture": {
+			Type: schema.TypeSet,
+			Elem: accountFeature,
+		},
+		"secureIdentityEntitlement": {
+			Type: schema.TypeSet,
+			Elem: accountFeature,
+		},
+		"secureThreatDetection": {
+			Type: schema.TypeSet,
+			Elem: accountFeature,
+		},
+		"secureAgentlessScanning": {
+			Type: schema.TypeSet,
+			Elem: accountFeature,
+		},
+		"monitorCloudMetrics": {
+			Type: schema.TypeSet,
+			Elem: accountFeature,
+		},
+	},
+}
+var accountFeature = &schema.Resource{
+	Schema: map[string]*schema.Schema{
+		"type": {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		"enabled": {
+			Type:     schema.TypeBool,
+			Optional: true,
+		},
+		"components": {
+			Type: schema.TypeList,
+			Elem: &schema.Schema{
+				Type: schema.TypeString,
+			},
+		},
+	},
 }
 
 func getSecureCloudauthAccountClient(client SysdigClients) (v2.CloudauthAccountSecureInterface, error) {
@@ -180,12 +229,153 @@ func resourceSysdigSecureCloudauthAccountDelete(ctx context.Context, data *schem
 }
 
 func cloudauthAccountFromResourceData(data *schema.ResourceData) *v2.CloudauthAccountSecure {
+	components := []*cloudauth.AccountComponent{}
+	for _, rc := range data.Get("components").([]interface{}) {
+		resourceComponent := rc.(map[string]interface{})
+		component := &cloudauth.AccountComponent{}
+		for key, value := range resourceComponent {
+			switch key {
+			case "type":
+				component.Type = cloudauth.Component(cloudauth.Component_value[value.(string)])
+			case "instance":
+				component.Instance = value.(string)
+			case "cloudConnectorMetadata":
+				cloudConnectorMetadata := &cloudauth.CloudConnectorMetadata{}
+				if err := protojson.Unmarshal([]byte(value.(string)), cloudConnectorMetadata); err == nil {
+					component.Metadata = &cloudauth.AccountComponent_CloudConnectorMetadata{
+						CloudConnectorMetadata: cloudConnectorMetadata,
+					}
+				}
+			case "trustedRoleMetadata":
+				metadata := &cloudauth.TrustedRoleMetadata{}
+				if err := protojson.Unmarshal([]byte(value.(string)), metadata); err == nil {
+					component.Metadata = &cloudauth.AccountComponent_TrustedRoleMetadata{
+						TrustedRoleMetadata: metadata,
+					}
+				}
+			case "eventBridgeMetadata":
+				metadata := &cloudauth.EventBridgeMetadata{}
+				if err := protojson.Unmarshal([]byte(value.(string)), metadata); err == nil {
+					component.Metadata = &cloudauth.AccountComponent_EventBridgeMetadata{
+						EventBridgeMetadata: metadata,
+					}
+				}
+			case "servicePrincipalMetadata":
+				metadata := &cloudauth.CloudConnectorMetadata{}
+				if err := protojson.Unmarshal([]byte(value.(string)), metadata); err == nil {
+					component.Metadata = &cloudauth.AccountComponent_CloudConnectorMetadata{
+						CloudConnectorMetadata: metadata,
+					}
+				}
+			case "webhookDatasourceMetadata":
+				metadata := &cloudauth.WebhookDatasourceMetadata{}
+				if err := protojson.Unmarshal([]byte(value.(string)), metadata); err == nil {
+					component.Metadata = &cloudauth.AccountComponent_WebhookDatasourceMetadata{
+						WebhookDatasourceMetadata: metadata,
+					}
+				}
+			case "cryptoKeyMetadata":
+				metadata := &cloudauth.CryptoKeyMetadata{}
+				if err := protojson.Unmarshal([]byte(value.(string)), metadata); err == nil {
+					component.Metadata = &cloudauth.AccountComponent_CryptoKeyMetadata{
+						CryptoKeyMetadata: metadata,
+					}
+				}
+			case "cloudLogsMetadata":
+				metadata := &cloudauth.CloudLogsMetadata{}
+				if err := protojson.Unmarshal([]byte(value.(string)), metadata); err == nil {
+					component.Metadata = &cloudauth.AccountComponent_CloudLogsMetadata{
+						CloudLogsMetadata: metadata,
+					}
+				}
+			}
+		}
+		components = append(components, component)
+	}
+
+	accountFeatures := &cloudauth.AccountFeatures{}
+	for name, value := range data.Get("feature").(map[string]interface{}) {
+		switch name {
+		case "secureConfigPosture":
+			accountFeatures.SecureConfigPosture = &cloudauth.AccountFeature{}
+			for name2, value2 := range value.(map[string]interface{}) {
+				switch name2 {
+				case "type":
+					accountFeatures.SecureConfigPosture.Type = cloudauth.Feature(cloudauth.Feature_value[value2.(string)])
+				case "enabled":
+					accountFeatures.SecureConfigPosture.Enabled = value2.(bool)
+				case "components":
+					for _, componentID := range value2.([]interface{}) {
+						accountFeatures.SecureConfigPosture.Components = append(accountFeatures.SecureConfigPosture.Components, componentID.(string))
+					}
+				}
+			}
+		case "secureIdentityEntitlement":
+			accountFeatures.SecureIdentityEntitlement = &cloudauth.AccountFeature{}
+			for name2, value2 := range value.(map[string]interface{}) {
+				switch name2 {
+				case "type":
+					accountFeatures.SecureIdentityEntitlement.Type = cloudauth.Feature(cloudauth.Feature_value[value2.(string)])
+				case "enabled":
+					accountFeatures.SecureIdentityEntitlement.Enabled = value2.(bool)
+				case "components":
+					for _, componentID := range value2.([]interface{}) {
+						accountFeatures.SecureIdentityEntitlement.Components = append(accountFeatures.SecureIdentityEntitlement.Components, componentID.(string))
+					}
+				}
+			}
+		case "secureThreatDetection":
+			accountFeatures.SecureThreatDetection = &cloudauth.AccountFeature{}
+			for name2, value2 := range value.(map[string]interface{}) {
+				switch name2 {
+				case "type":
+					accountFeatures.SecureThreatDetection.Type = cloudauth.Feature(cloudauth.Feature_value[value2.(string)])
+				case "enabled":
+					accountFeatures.SecureThreatDetection.Enabled = value2.(bool)
+				case "components":
+					for _, componentID := range value2.([]interface{}) {
+						accountFeatures.SecureThreatDetection.Components = append(accountFeatures.SecureThreatDetection.Components, componentID.(string))
+					}
+				}
+			}
+		case "secureAgentlessScanning":
+			accountFeatures.SecureAgentlessScanning = &cloudauth.AccountFeature{}
+			for name2, value2 := range value.(map[string]interface{}) {
+				switch name2 {
+				case "type":
+					accountFeatures.SecureAgentlessScanning.Type = cloudauth.Feature(cloudauth.Feature_value[value2.(string)])
+				case "enabled":
+					accountFeatures.SecureAgentlessScanning.Enabled = value2.(bool)
+				case "components":
+					for _, componentID := range value2.([]interface{}) {
+						accountFeatures.SecureAgentlessScanning.Components = append(accountFeatures.SecureAgentlessScanning.Components, componentID.(string))
+					}
+				}
+			}
+		case "monitorCloudMetrics":
+			accountFeatures.MonitorCloudMetrics = &cloudauth.AccountFeature{}
+			for name2, value2 := range value.(map[string]interface{}) {
+				switch name2 {
+				case "type":
+					accountFeatures.MonitorCloudMetrics.Type = cloudauth.Feature(cloudauth.Feature_value[value2.(string)])
+				case "enabled":
+					accountFeatures.MonitorCloudMetrics.Enabled = value2.(bool)
+				case "components":
+					for _, componentID := range value2.([]interface{}) {
+						accountFeatures.MonitorCloudMetrics.Components = append(accountFeatures.MonitorCloudMetrics.Components, componentID.(string))
+					}
+				}
+			}
+		}
+	}
+
 	return &v2.CloudauthAccountSecure{
 		CloudAccount: cloudauth.CloudAccount{
 			Enabled:    data.Get("enabled").(bool),
 			ProviderId: data.Get("cloud_provider_id").(string),
 			Provider:   cloudauth.Provider(cloudauth.Provider_value[data.Get("cloud_provider_type").(string)]),
-			Components: []*cloudauth.AccountComponent{},
+			Components: components,
+			Feature:    accountFeatures,
 		},
 	}
 }
