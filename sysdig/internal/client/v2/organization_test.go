8b1c41ba818d3ee6d0f9430aa687277b63f6d3cc
Mandar Kulkarni <mandar.kulkarni@sysdig.com>
2023-09-25 13:45:08 -0700

    Add test code for organization resource

---
+++ sysdig/internal/client/v2/organization_test.go
@@ -0,0 +1,60 @@
+//go:build unit
+
+package v2
+
+import (
+	cloudauth "github.com/draios/terraform-provider-sysdig/sysdig/internal/client/v2/cloudauth/go"
+	"io"
+	"strings"
+	"testing"
+)
+
+func TestMarshalOrg(t *testing.T) {
+	t.Parallel()
+	c := Client{}
+
+	given := &OrganizationSecure{
+		cloudauth.CloudOrganization{
+			ManagementAccountId: "58ca66a5-ac87-497b-a501-7a4c934b3017",
+			Provider:            cloudauth.Provider_PROVIDER_GCP,
+		},
+	}
+	expected := `{"managementAccountId":"58ca66a5-ac87-497b-a501-7a4c934b3017","provider":"PROVIDER_GCP"}`
+
+	payload, err := c.marshalOrg(given)
+	if err != nil {
+		t.Errorf("failed to marshal payload, err: %v", err)
+	}
+
+	buf := &strings.Builder{}
+	_, err = io.Copy(buf, payload)
+	if err != nil {
+		t.Errorf("failed to populate buffer, err: %v", err)
+	}
+	marshaled := buf.String()
+
+	if marshaled != expected {
+		t.Errorf("expected %v, got %v", expected, marshaled)
+	}
+}
+
+func TestUnmarshalOrg(t *testing.T) {
+	t.Parallel()
+	c := Client{}
+	given := `{"managementAccountId":"58ca66a5-ac87-497b-a501-7a4c934b3017","provider":"PROVIDER_GCP"}`
+	expected := &OrganizationSecure{
+		cloudauth.CloudOrganization{
+			ManagementAccountId: "58ca66a5-ac87-497b-a501-7a4c934b3017",
+			Provider:            cloudauth.Provider_PROVIDER_GCP,
+		},
+	}
+
+	unmarshalled, err := c.unmarshalOrg(io.NopCloser(strings.NewReader(given)))
+	if err != nil {
+		t.Errorf("got error while unmarshaling, err: %v", err)
+	}
+
+	if expected.String() != unmarshalled.String() {
+		t.Errorf("expected %v, got %v", expected, unmarshalled)
+	}
+}
