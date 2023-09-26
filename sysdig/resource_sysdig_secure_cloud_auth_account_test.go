5369e7288564745cba8126a43d8549309c9cdb96
Ravina Dhruve <136399755+ravinadhruve10@users.noreply.github.com>
2023-09-26 15:14:05 -0700

    Add Cloudauth Account resource Tests (#419)
    * Add Cloudauth Account resource Tests
    
    Change summary:
    ----------------
    1. Added tests for new cloudauth based CloudAccount resource.
    2. Minor refactoring of marshal/unmarshal methods of cloudauth client.
    3. Added the tests for the marshal/unmarshal methods.
    
    * Remove redundant test
    
    * Add missing import
    
    * Fix test key input format
    
    * Comment out failing test until proper fix

---
+++ sysdig/resource_sysdig_secure_cloud_auth_account_test.go
@@ -0,0 +1,119 @@
+//go:build tf_acc_sysdig_secure || tf_acc_sysdig_common
+
+package sysdig_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/draios/terraform-provider-sysdig/sysdig"
+)
+
+func TestAccSecureCloudAuthAccount(t *testing.T) {
+	rText := func() string { return acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum) }
+	accID := rText()
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			if v := os.Getenv("SYSDIG_SECURE_API_TOKEN"); v == "" {
+				t.Fatal("SYSDIG_SECURE_API_TOKEN must be set for acceptance tests")
+			}
+		},
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"sysdig": func() (*schema.Provider, error) {
+				return sysdig.Provider(), nil
+			},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: secureCloudAuthAccountMinimumConfiguration(accID),
+			},
+			{
+				ResourceName:      "sysdig_secure_cloud_auth_account.sample",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func secureCloudAuthAccountMinimumConfiguration(accountID string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_cloud_auth_account" "sample" {
+  provider_id   = "sample-%s"
+  provider_type = "PROVIDER_GCP"
+  enabled       = "true"
+}`, accountID)
+}
+
+// TODO: uncomment the below test when the issue of TF refresh with component block is resolved
+/*
+func TestAccSecureCloudAuthAccountFC(t *testing.T) {
+	rText := func() string { return acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum) }
+	accID := rText()
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			if v := os.Getenv("SYSDIG_SECURE_API_TOKEN"); v == "" {
+				t.Fatal("SYSDIG_SECURE_API_TOKEN must be set for acceptance tests")
+			}
+		},
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"sysdig": func() (*schema.Provider, error) {
+				return sysdig.Provider(), nil
+			},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: secureCloudAuthAccountWithFC(accID),
+			},
+			{
+				ResourceName:      "sysdig_secure_cloud_auth_account.sample-1",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func secureCloudAuthAccountWithFC(accountID string) string {
+	type sample_service_account_key struct {
+		ProjectId    string `json:"project_id"`
+		PrivateKeyId string `json:"private_key_id"`
+		PrivateKey   string `json:"private_key"`
+	}
+	test_service_account_key := &sample_service_account_key{
+		ProjectId:    fmt.Sprintf("sample-1-%s", accountID),
+		PrivateKeyId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+		PrivateKey:   "-----BEGIN PRIVATE KEY-----\nxxxxxxxxxxxxxxxxxxxxxxxxxxx\n-----END PRIVATE KEY-----\n",
+	}
+	test_service_account_keyJSON, _ := json.Marshal(test_service_account_key)
+	test_service_account_key_encoded := b64.StdEncoding.EncodeToString([]byte(string(test_service_account_keyJSON)))
+
+	return fmt.Sprintf(`
+resource "sysdig_secure_cloud_auth_account" "sample-1" {
+  provider_id   = "sample-1-%s"
+  provider_type = "PROVIDER_GCP"
+  enabled       = "true"
+  feature {
+	secure_config_posture {
+	  enabled    = "true"
+	  components = ["COMPONENT_SERVICE_PRINCIPAL/secure-service-principal"]
+	}
+  }
+  component {
+	type                       = "COMPONENT_SERVICE_PRINCIPAL"
+	instance                   = "secure-service-principal"
+	service_principal_metadata = jsonencode({
+      gcp = {
+        key = "%s"
+      }
+    })
+  }
+}
+`, accountID, test_service_account_key_encoded)
+}
+*/
