5d7f762638f14f690e6385853d29092a4983ef39
Grzegorz Nosek <root@localdomain.pl>
2023-10-27 17:18:43 +0200

    fix(tests): compare the whole instrumented container definitions in ECS test

--- sysdig/data_source_sysdig_fargate_ECS_test.go
+++ sysdig/data_source_sysdig_fargate_ECS_test.go
@@ -198,46 +198,7 @@ func TestECStransformation(t *testing.T) {
 		t.Fatalf("Cannot find testfiles/ECSinput.json")
 	}
 
-	type cdef struct {
-		Command          []string            `json:"Command"`
-		EntryPoint       []string            `json:"EntryPoint"`
-		Environment      []map[string]string `json:"Environment"`
-		Image            string              `json:"Image"`
-		Linuxparameters  interface{}         `json:"LinuxParameters"`
-		VolumesFrom      []interface{}       `json:"VolumesFrom"`
-		LogConfiguration interface{}         `json:"LogConfiguration"`
-		Name             string              `json:"Name"`
-		Name2            string              `json:"name"`
-		Image2           string              `json:"image"`
-		EntryPoint2      string              `json:"entryPoint"`
-	}
-
-	var patchedContainerDefinitions, expectedContainerDefinitions []cdef
-	err = json.Unmarshal([]byte(*patchedOutput), &patchedContainerDefinitions)
-	if err != nil {
-		t.Fatalf("Error Unmarshaling patched Container definitions : %v", err.Error())
-	}
-
-	err = json.Unmarshal([]byte(expectedOutput), &expectedContainerDefinitions)
-	if err != nil {
-		t.Fatalf("Error Unmarshaling expected Container definitions: %v", err.Error())
-	}
-
-	// Check if Name key is correct
-	assert.Equal(t, expectedContainerDefinitions[0].Name, patchedContainerDefinitions[0].Name)
-	assert.Equal(t, expectedContainerDefinitions[0].Name2, "")
-
-	// The order received from patchedOutput changes continuously hence it is important to check if the arrays of expected and actual are equal without order being correct. This check also
-	// helps with checking if key/value is named "Name" and "Value" accordingly.
-	assert.ElementsMatch(t, expectedContainerDefinitions[0].Environment, patchedContainerDefinitions[0].Environment)
-
-	// Check if Image key is correct
-	assert.Equal(t, expectedContainerDefinitions[0].Image, patchedContainerDefinitions[0].Image)
-	assert.Equal(t, patchedContainerDefinitions[0].Image2, "")
-
-	// Check if entryPoint key is correct
-	assert.Equal(t, expectedContainerDefinitions[0].EntryPoint, patchedContainerDefinitions[0].EntryPoint)
-	assert.Equal(t, patchedContainerDefinitions[0].EntryPoint2, "")
+	sortAndCompare(t, expectedOutput, []byte(*patchedOutput))
 }
 
 func TestPatchFargateTaskDefinition(t *testing.T) {
