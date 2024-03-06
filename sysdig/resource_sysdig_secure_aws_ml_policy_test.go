43bd1d39e5d2ea8c3f3040ebb4f8926ac8679edb
Ben Rosenbloom <benjamin.rosenbloom@sysdig.com>
2024-03-06 18:30:01 -0500

    Fix imports

--- sysdig/resource_sysdig_secure_aws_ml_policy_test.go
+++ sysdig/resource_sysdig_secure_aws_ml_policy_test.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/draios/terraform-provider-sysdig/buildinfo"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
