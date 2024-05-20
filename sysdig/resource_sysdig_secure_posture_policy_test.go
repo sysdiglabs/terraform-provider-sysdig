b8415a4a8c52a095046b27f06f5dabb1e62c4bd4
hila1608 <hila.krut@sysdig.com>
2024-05-20 15:02:39 +0300

    remove ibm env from test

--- sysdig/resource_sysdig_secure_posture_policy_test.go
+++ sysdig/resource_sysdig_secure_posture_policy_test.go
@@ -16,7 +16,7 @@ import (
 func TestSecurePosturePolicy(t *testing.T) {
 	rText := func() string { return acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum) }
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: preCheckAnyEnv(t, SysdigSecureApiTokenEnv, SysdigIBMSecureAPIKeyEnv),
+		PreCheck: preCheckAnyEnv(t, SysdigSecureApiTokenEnv),
 		ProviderFactories: map[string]func() (*schema.Provider, error){
 			"sysdig": func() (*schema.Provider, error) {
 				return sysdig.Provider(), nil
