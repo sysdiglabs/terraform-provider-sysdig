4e365188b1460bc612c9da5c6e8d93d554133eba
cgeers <christopher.geers@sysdig.com>
2024-06-06 16:24:36 -0500

    feat(datasource): agentless scanning assets (#516)

--- sysdig/data_source_sysdig_secure_onboarding_test.go
+++ sysdig/data_source_sysdig_secure_onboarding_test.go
@@ -81,3 +81,29 @@ func TestAccTenantExternalIDDataSource(t *testing.T) {
 		},
 	})
 }
+
+func TestAccAgentlessScanningAssetsDataSource(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			if v := os.Getenv("SYSDIG_SECURE_API_TOKEN"); v == "" {
+				t.Fatal("SYSDIG_SECURE_API_TOKEN must be set for acceptance tests")
+			}
+		},
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"sysdig": func() (*schema.Provider, error) {
+				return sysdig.Provider(), nil
+			},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: `data "sysdig_secure_agentless_scanning_assets" "assets" {}`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.sysdig_secure_agentless_scanning_assets.assets", "aws.%", "1"),
+					resource.TestCheckResourceAttr("data.sysdig_secure_agentless_scanning_assets.assets", "azure.%", "2"),
+					resource.TestCheckResourceAttr("data.sysdig_secure_agentless_scanning_assets.assets", "backend.%", "2"),
+					resource.TestCheckResourceAttr("data.sysdig_secure_agentless_scanning_assets.assets", "gcp.%", "1"),
+				),
+			},
+		},
+	})
+}
