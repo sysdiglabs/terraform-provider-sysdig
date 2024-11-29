ffacd11091da63a518a9c07428b22d463d4e58af
Fede Barcelona <fede_rico_94@hotmail.com>
2024-11-29 12:17:03 +0100

    chore(nix): add flake for reproducible development environment

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    let
+      overlays.default = final: prev: { };
+      flake = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [ self.overlays.default ];
+          };
+        in
+        {
+          packages = with pkgs; {
+            inherit terraform-provider-sysdig;
+            default = terraform-provider-sysdig;
+          };
+          devShells.default = with pkgs; mkShell { packages = [ ]; };
+
+          formatter = pkgs.nixfmt-rfc-style;
+        }
+      );
+    in
+    flake // { inherit overlays; };
+}
