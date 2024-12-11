7893a312a203d6597754a7763a30d4809f69ec41
Fede Barcelona <fede_rico_94@hotmail.com>
2024-12-11 10:42:55 +0100

    build(nix): use 1.0.0-local version in the nix package

--- package.nix
+++ package.nix
@@ -1,11 +1,20 @@
 {
   terraform-providers,
 }:
+# Allows testing of the provider with the following block:
+# terraform {
+#   required_providers {
+#     sysdig = {
+#       source  = "sysdiglabs/sysdig"
+#       version = "=1.0.0-local"
+#     }
+#   }
+# }
 terraform-providers.mkProvider {
   owner = "sysdiglabs";
   repo = "terraform-provider-sysdig";
   homepage = "https://registry.terraform.io/providers/sysdiglabs/sysdig";
-  rev = "master";
+  rev = "1.0.0-local"; # Keeping this version fixed with a `-local` version, so user can just bundle the concrete plugin version with terraform using nix.
   vendorHash = "sha256-9ru4RkH2fDWcgM0I3URlWd811PwySktd+gLsEr624WM=";
   hash = "";
   mkProviderFetcher = { ... }: ./.;
