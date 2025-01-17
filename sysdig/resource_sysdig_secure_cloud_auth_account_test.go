5386606b4face7398f29d1952f9f0ba2de2e286e
jose-pablo-camacho <jose.camacho@sysdig.com>
2025-01-17 13:32:13 -0600

    SSPROD-51715 - add(oci): key metadata (#592)
    * add(oci): key metadata
    
    * add(oci): key metadata

--- sysdig/resource_sysdig_secure_cloud_auth_account_test.go
+++ sysdig/resource_sysdig_secure_cloud_auth_account_test.go
@@ -755,6 +755,8 @@ resource "sysdig_secure_cloud_auth_account" "sample" {
 			api_key = {
 			  user_id = "user-id"
 #			  region = "region"
+#			  fingerprint = "fingerprint"
+#			  private_key = "private_key"
 			}
 			policy = {
 			 policy_id = "policy-id"
