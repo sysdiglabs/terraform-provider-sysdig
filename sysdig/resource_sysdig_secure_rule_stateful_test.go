67b426e2854147feed194f6c0ab26f9aa72411f9
kmvachhani <krishna.vachhani@sysdig.com>
2025-02-25 11:03:54 -0800

    Update resource_sysdig_secure_rule_stateful_test.go

--- sysdig/resource_sysdig_secure_rule_stateful_test.go
+++ sysdig/resource_sysdig_secure_rule_stateful_test.go
@@ -1,18 +1,22 @@
 package sysdig_test
 
 import (
+	"os"
 	"testing"
 
+	"github.com/draios/terraform-provider-sysdig/sysdig"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func TestRuleStatefulAppends(t *testing.T) {
 	steps := []resource.TestStep{
 		{
-			Config: ruleStatefulAppend(randomString()),
+			Config: ruleStatefulAppend(rName()),
 		},
 	}
-	runTest(steps, t)
+	runStatefulTest(steps, t)
 }
 
 func ruleStatefulAppend(name string) string {
@@ -27,4 +31,23 @@ func ruleStatefulAppend(name string) string {
       name = "tf_append_%s"
     }
 	}`
+}
+
+func rName() string { return acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum) }
+
+func runStatefulTest(steps []resource.TestStep, t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			if v := os.Getenv("SYSDIG_SECURE_API_TOKEN"); v == "" {
+				t.Fatal("SYSDIG_SECURE_API_TOKEN must be set for acceptance tests")
+			}
+		},
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"sysdig": func() (*schema.Provider, error) {
+				return sysdig.Provider(), nil
+			},
+		},
+		Steps: steps,
+	})
+
 }
