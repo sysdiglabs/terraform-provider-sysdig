ceb309606b6c4d624f77a24e2236b4e6a521c64b
vojindjukic <vojin.djukic@sysdig.com>
2025-04-16 10:50:28 +0200

    add tests for IBM

---
+++ sysdig/data_source_sysdig_monitor_team_ibm_test.go
@@ -0,0 +1,53 @@
+//go:build tf_acc_ibm_monitor
+
+package sysdig_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/draios/terraform-provider-sysdig/sysdig"
+)
+
+func TestAccDataSourceSysdigMonitorTeam(t *testing.T) {
+	name := fmt.Sprintf("test-monitor-team-%s", randomText(5))
+	resource.Test(t, resource.TestCase{
+		PreCheck: preCheckAnyEnv(t, SysdigIBMMonitorAPIKeyEnv),
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"sysdig": func() (*schema.Provider, error) {
+				return sysdig.Provider(), nil
+			},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: monitorTeamResourceAndDatasource(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.sysdig_monitor_team.test_dt", "name", name),
+					resource.TestCheckResourceAttr("data.sysdig_monitor_team.test_dt", "enable_ibm_platform_metrics", "true"),
+					resource.TestCheckResourceAttr("data.sysdig_monitor_team.test_dt", "ibm_platform_metrics", "foo in (\"0\") and bar in (\"3\")"),
+				),
+			},
+		},
+	})
+}
+
+func monitorTeamWithPlatformMetricsIBM(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_monitor_team" "test" {
+  name = "%s"
+  enable_ibm_platform_metrics = true
+  ibm_platform_metrics = "foo in (\"0\") and bar in (\"3\")"
+
+  entrypoint {
+	type = "Dashboards"
+  }
+}
+
+data "sysdig_monitor_team" "test_dt" {
+  id = sysdig_monitor_team.sample.id
+}
+`, name)
+}
--- sysdig/data_source_sysdig_secure_team_test.go
+++ sysdig/data_source_sysdig_secure_team_test.go
@@ -1,4 +1,4 @@
-//go:build tf_acc_sysdig_secure || tf_acc_onprem_secure || tf_acc_ibm_secure
+//go:build tf_acc_sysdig_secure || tf_acc_onprem_secure
 
 package sysdig_test
 
@@ -14,7 +14,7 @@ import (
 func TestAccDataSourceSysdigSecureTeam(t *testing.T) {
 	name := fmt.Sprintf("test-secure-team-%s", randomText(5))
 	resource.Test(t, resource.TestCase{
-		PreCheck: preCheckAnyEnv(t, SysdigSecureApiTokenEnv, SysdigIBMSecureAPIKeyEnv),
+		PreCheck: preCheckAnyEnv(t, SysdigSecureApiTokenEnv),
 		ProviderFactories: map[string]func() (*schema.Provider, error){
 			"sysdig": func() (*schema.Provider, error) {
 				return sysdig.Provider(), nil
@@ -25,6 +25,14 @@ func TestAccDataSourceSysdigSecureTeam(t *testing.T) {
 				Config: secureTeamAndDatasource(name),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.sysdig_secure_team.test", "name", name),
+					resource.TestCheckResourceAttr("data.sysdig_secure_team.test", "description", "A test secure team"),
+					resource.TestCheckResourceAttr("data.sysdig_secure_team.test", "scope_by", "container"),
+					resource.TestCheckResourceAttr("data.sysdig_secure_team.test", "filter", "container.image.repo = \"sysdig/agent\""),
+					resource.TestCheckResourceAttr("data.sysdig_secure_team.test", "version", "0"),
+					resource.TestCheckResourceAttr("data.sysdig_secure_team.test", "can_use_sysdig_capture", "true"),
+					resource.TestCheckResourceAttr("data.sysdig_secure_team.test", "can_see_infrastructure_events", "true"),
+					resource.TestCheckResourceAttr("data.sysdig_secure_team.test", "can_use_aws_data", "false"),
+					resource.TestCheckResourceAttr("data.sysdig_secure_team.test", "all_zones", "true"),
 				),
 			},
 		},
@@ -34,10 +42,14 @@ func TestAccDataSourceSysdigSecureTeam(t *testing.T) {
 func secureTeamAndDatasource(name string) string {
 	return fmt.Sprintf(`
 resource "sysdig_secure_team" "sample" {
-  name        = "%s"
+  name               = "%s"
   description        = "A test secure team"
   scope_by           = "container"
+  can_use_sysdig_capture = true
+  can_see_infrastructure_events = true
   filter             = "container.image.repo = \"sysdig/agent\""
+  can_use_aws_data   = false
+  all_zones          = true
 }
 
 data "sysdig_secure_team" "test" {
