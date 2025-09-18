f4d7963a4ff019ea5cae2a7536b3863e570ee59d
Fede Barcelona <fede_rico_94@hotmail.com>
2025-09-18 09:33:57 +0200

    refactor!: remove deprecated sysdig_monitor_alert_promql resource

--- sysdig/provider.go
+++ sysdig/provider.go
@@ -126,7 +126,6 @@ func (p *SysdigProvider) Provider() *schema.Provider {
 
 			"sysdig_monitor_alert_event":         deprecatedResourceSysdigMonitorAlertEvent(),
 			"sysdig_monitor_alert_group_outlier": deprecatedResourceSysdigMonitorAlertGroupOutlier(),
-			"sysdig_monitor_alert_promql":        deprecatedResourceSysdigMonitorAlertPromql(),
 
 			"sysdig_monitor_alert_v2_change":                               resourceSysdigMonitorAlertV2Change(),
 			"sysdig_monitor_alert_v2_downtime":                             resourceSysdigMonitorAlertV2Downtime(),
