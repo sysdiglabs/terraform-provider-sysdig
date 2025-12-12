90bef3fd5361d118e477bd9ffc0629e2df95a188
Fede Barcelona <fede_rico_94@hotmail.com>
2025-12-12 09:27:01 +0100

    fix: prevent panic on missing secure_custom_policy

--- sysdig/resource_sysdig_secure_custom_policy.go
+++ sysdig/resource_sysdig_secure_custom_policy.go
@@ -193,6 +193,7 @@ func resourceSysdigCustomPolicyRead(ctx context.Context, d *schema.ResourceData,
 	if err != nil {
 		if statusCode == http.StatusNotFound {
 			d.SetId("")
+			return nil
 		} else {
 			return diag.FromErr(err)
 		}
