283718ab7860da033b80cd9603682c8d775a1036
Fede Barcelona <fede_rico_94@hotmail.com>
2025-12-12 09:29:48 +0100

    fix: prevent panic on missing secure_rule_process

--- sysdig/resource_sysdig_secure_rule_process.go
+++ sysdig/resource_sysdig_secure_rule_process.go
@@ -86,6 +86,7 @@ func resourceSysdigRuleProcessRead(ctx context.Context, d *schema.ResourceData, 
 	if err != nil {
 		if statusCode == http.StatusNotFound {
 			d.SetId("")
+			return nil
 		} else {
 			return diag.FromErr(err)
 		}
