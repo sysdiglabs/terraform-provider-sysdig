0fcc6450785c065bb56ca94486b162392d880fb5
Fede Barcelona <fede_rico_94@hotmail.com>
2025-12-12 09:30:15 +0100

    fix: prevent panic on missing secure_rule_syscall

--- sysdig/resource_sysdig_secure_rule_syscall.go
+++ sysdig/resource_sysdig_secure_rule_syscall.go
@@ -85,6 +85,7 @@ func resourceSysdigRuleSyscallRead(ctx context.Context, d *schema.ResourceData, 
 	if err != nil {
 		if statusCode == http.StatusNotFound {
 			d.SetId("")
+			return nil
 		} else {
 			return diag.FromErr(err)
 		}
