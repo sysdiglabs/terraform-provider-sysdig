2337c42d160686d141778340e6685b3ccec1b0a9
Ben Rosenbloom <benjamin.rosenbloom@sysdig.com>
2025-12-17 11:16:03 -0500

    Revert "Removed List Matching rules tests and updated documentation"
    This reverts commit b5bd6d9d698da3997f37e7cd677b2f945f7fadb4.

---
+++ sysdig/resource_sysdig_secure_rule_network_test.go
@@ -0,0 +1,165 @@
+//go:build tf_acc_sysdig_secure || tf_acc_policies || tf_acc_onprem_secure
+
+package sysdig_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/draios/terraform-provider-sysdig/sysdig"
+)
+
+func TestAccRuleNetwork(t *testing.T) {
+	rText := func() string { return acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum) }
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			if v := os.Getenv("SYSDIG_SECURE_API_TOKEN"); v == "" {
+				t.Fatal("SYSDIG_SECURE_API_TOKEN must be set for acceptance tests")
+			}
+		},
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"sysdig": func() (*schema.Provider, error) {
+				return sysdig.Provider(), nil
+			},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: ruleNetworkWithName(rText()),
+			},
+			{
+				Config: ruleNetworkWithoutTags(rText()),
+			},
+			{
+				Config: ruleNetworkWithTCP(rText()),
+			},
+			{
+				Config: ruleNetworkWithUDP(rText()),
+			},
+			{
+				Config: ruleNetworkAllowingAllTraffic(rText()),
+			},
+			{
+				ResourceName:      "sysdig_secure_rule_network.foo",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: ruleNetworkWithMinimalConfig(rText()),
+			},
+		},
+	})
+}
+
+func ruleNetworkWithName(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_rule_network" "foo" {
+  name = "TERRAFORM TEST %s" // ID
+  description = "TERRAFORM TEST %s"
+  tags = ["network", "cis"]
+
+  block_inbound = true
+  block_outbound = true
+
+  tcp {
+    matching = true // default
+    ports = [80, 443]
+  }
+
+  udp {
+    matching = true // default
+    ports = [80, 443]
+  }
+}`, name, name)
+}
+
+func ruleNetworkAllowingAllTraffic(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_rule_network" "foo" {
+  name = "TERRAFORM TEST %s" // ID
+  description = "TERRAFORM TEST %s"
+  tags = ["network", "cis"]
+
+  block_inbound = false
+  block_outbound = false
+
+  tcp {
+    matching = true // default
+    ports = [80, 443]
+  }
+
+  udp {
+    matching = true // default
+    ports = [80, 443]
+  }
+}`, name, name)
+}
+
+func ruleNetworkWithoutTags(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_rule_network" "foo" {
+  name = "TERRAFORM TEST %s" // ID
+  description = "TERRAFORM TEST %s"
+
+  block_inbound = true
+  block_outbound = true
+
+  tcp {
+    matching = true // default
+    ports = [80, 443]
+  }
+
+  udp {
+    matching = true // default
+    ports = [80, 443]
+  }
+}`, name, name)
+}
+
+func ruleNetworkWithTCP(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_rule_network" "foo" {
+  name = "TERRAFORM TEST %s" // ID
+  description = "TERRAFORM TEST %s"
+
+  block_inbound = true
+  block_outbound = true
+
+  tcp {
+    matching = true // default
+    ports = [80, 443]
+  }
+}`, name, name)
+}
+
+func ruleNetworkWithUDP(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_rule_network" "foo" {
+  name = "TERRAFORM TEST %s" // ID
+  description = "TERRAFORM TEST %s"
+
+  block_inbound = true
+  block_outbound = true
+
+  udp {
+    matching = true // default
+    ports = [80, 443]
+  }
+}`, name, name)
+}
+
+func ruleNetworkWithMinimalConfig(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_rule_network" "foo-minimal" {
+  name = "TERRAFORM TEST %s" // ID
+  description = "TERRAFORM TEST %s"
+
+  block_inbound = true
+  block_outbound = true
+}`, name, name)
+}
---
+++ sysdig/resource_sysdig_secure_rule_syscall_test.go
@@ -0,0 +1,80 @@
+//go:build tf_acc_sysdig_secure || tf_acc_policies || tf_acc_onprem_secure
+
+package sysdig_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/draios/terraform-provider-sysdig/sysdig"
+)
+
+func TestAccRuleSyscall(t *testing.T) {
+	rText := func() string { return acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum) }
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			if v := os.Getenv("SYSDIG_SECURE_API_TOKEN"); v == "" {
+				t.Fatal("SYSDIG_SECURE_API_TOKEN must be set for acceptance tests")
+			}
+		},
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"sysdig": func() (*schema.Provider, error) {
+				return sysdig.Provider(), nil
+			},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: ruleSyscallWithName(rText()),
+			},
+			{
+				ResourceName:      "sysdig_secure_rule_syscall.foo",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: ruleSyscallWithoutTags(rText()),
+			},
+			{
+				Config: ruleSyscallWithMinimalConfig(rText()),
+			},
+		},
+	})
+}
+
+func ruleSyscallWithName(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_rule_syscall" "foo" {
+  name = "TERRAFORM TEST %s"
+  description = "TERRAFORM TEST %s"
+  tags = ["syscall", "cis"]
+
+  matching = true // default
+  syscalls = ["open", "execve"]
+}`, name, name)
+}
+
+func ruleSyscallWithoutTags(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_rule_syscall" "foo" {
+  name = "TERRAFORM TEST %s"
+  description = "TERRAFORM TEST %s"
+
+  matching = true // default
+  syscalls = ["open", "execve"]
+}`, name, name)
+}
+
+func ruleSyscallWithMinimalConfig(name string) string {
+	return fmt.Sprintf(`
+resource "sysdig_secure_rule_syscall" "foo-minimal" {
+  name = "TERRAFORM TEST %s"
+  description = "TERRAFORM TEST %s"
+
+}`, name, name)
+}
